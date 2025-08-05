2439d77a063eb45b509300972f49bae2424a8244
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-08-05 12:43:36 +0500

    fix

--- core/apps/cars/views/like.py
+++ core/apps/cars/views/like.py
@@ -34,7 +34,7 @@ class LikedCarListApiView(generics.ListAPIView):
         ).select_related(
             'fuel_type', 'color'
         ).prefetch_related(
-            'car_medias'
+            'car_medias', 'likes', 'comparisons'
         ).distinct()
 
 
@@ -63,4 +63,4 @@ class ComparisonListApiView(generics.ListAPIView):
     def get_queryset(self):
         return Car.objects.filter(comparisons__user=self.request.user).select_related(
             'fuel_type', 'color',
-        ).prefetch_related('car_medias').distinct()
+        ).prefetch_related('car_medias', 'likes', 'comparisons').distinct()
