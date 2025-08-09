e879fab0140cf2fdee7b123187bbca3af7c873b2
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-08-09 15:41:23 +0500

    fix

--- core/apps/common/urls.py
+++ core/apps/common/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from core.apps.common.views.main import AboutProjectListApiView, ContactListApiView, PartnerListApiView, FeedBackListApiView, DisclaimerListApiView, ReturnPolicyListApiView, PrivacyPolicyListApiView, OfferAgreementListApiView, OrderCarImportListApiView, SecureTransactionListApiView
-
+from core.apps.common.views.main import AboutProjectListApiView, ContactListApiView, PartnerListApiView, FeedBackListApiView, DisclaimerListApiView, ReturnPolicyListApiView, PrivacyPolicyListApiView, OfferAgreementListApiView, OrderCarImportListApiView, SecureTransactionListApiView, FAQListApiView
 
 urlpatterns = [
     path('about_projects/', AboutProjectListApiView.as_view()),
@@ -14,4 +13,5 @@ urlpatterns = [
     path('offer_agreement/', OfferAgreementListApiView.as_view()),
     path('order_car_import/', OrderCarImportListApiView.as_view()),
     path('secure_transaction/', SecureTransactionListApiView.as_view()),
+    path('faq/', FAQListApiView.as_view()),
 ]
