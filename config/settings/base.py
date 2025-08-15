5603dec1a49297584a7cc0154e85b53129348bf6
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-08-15 22:49:52 +0500

    fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -161,14 +161,14 @@ REST_FRAMEWORK = {
 
     # 'DEFAULT_PAGINATION_CLASS': 'core.apps.shared.paginations.custom.CustomPageNumberPagination',
     # 'PAGE_SIZE': 10,
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle',
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/minute',  
-        'user': '100/minute',
-    },
+    # 'DEFAULT_THROTTLE_CLASSES': [
+    #     'rest_framework.throttling.AnonRateThrottle',
+    #     'rest_framework.throttling.UserRateThrottle',
+    # ],
+    # 'DEFAULT_THROTTLE_RATES': {
+    #     'anon': '100/minute',  
+    #     'user': '100/minute',
+    # },
 }
 
 from datetime import timedelta
