72aa540a33d5ba00ff50cb0af206a742aaa33fe4
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-08-24 16:41:04 +0500

    remove unused api

--- core/apps/admin_panel/urls.py
+++ core/apps/admin_panel/urls.py
@@ -38,10 +38,9 @@ urlpatterns = [
         ]
     )),
     # full -> post, delete, patch, get, get
-    path('model/', include(
+c    path('model/', include(
         [
             path('<uuid:brand_id>/list/', model_views.AdminModelByBrandListApiView.as_view()), 
-            path('list/', model_views.AdminModelListApiView.as_view()),
             path('create/', model_views.AdminModelCreateApiView.as_view()),
             path('<uuid:id>/update/', model_views.AdminModelUpdateApiView.as_view()),
             path('<uuid:id>/delete/', model_views.AdminModelDeleteApiView.as_view()),
@@ -51,7 +50,6 @@ urlpatterns = [
     path('generation/', include(
         [
             path('<uuid:model_id>/list/', generation_views.AdminGenerationListByModelApiView.as_view()),
-            path('list/', generation_views.AdminGenerationListApiView.as_view()),
             path('create/', generation_views.AdminGenerationCreateApiView.as_view()),
             path('<uuid:id>/update/', generation_views.AdminGenerationUpdateApiView.as_view()),
             path('<uuid:id>/delete/', generation_views.AdminGenerationDeleteApiView.as_view()),
